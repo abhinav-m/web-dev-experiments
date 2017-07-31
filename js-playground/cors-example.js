4039919ef771af703ba2131bdc608c8adeb1df53
Abhinav Mishra <abhinav.thinktank@gmail.com>
2017-07-31 20:40:22 +0530

    Added JSONP example via vanilla JS

--- js-playground/cors-example.js
+++ js-playground/cors-example.js
@@ -5,22 +5,16 @@ function initAll() {
         navigator.geolocation.getCurrentPosition(makeCall)
     } else
         alert("your browser doesn't support this!");
+}
 
-    function makeCall(position) {
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
-        var url = "https://api.darksky.net/forecast/b185ccffd3fe5dc262bda8424f07ee7e/" + latitude + "," + longitude;
-        var httpReq = new XMLHttpRequest();
-        httpReq.open('GET', url, true);
-        // httpReq.setRequestHeader('origin', 'Access-Control-Allow-Origin');
-        httpReq.setRequestHeader("Content-Type", "application/json");
-        httpReq.onreadystatechange = callback;
-        httpReq.send();
+function makeCall(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var script = document.createElement("script");
+    script.src = 'https://api.github.com/gists?callback=myCallBack';
+    document.body.appendChild(script);
+}
 
-        function callback() {
-            document.getElementById("content").innerHTML = httpReq.responseText;
-
-        }
-    }
-
+function myCallBack(response) {
+    document.getElementById("content").innerHTML = response.toString();
 }
