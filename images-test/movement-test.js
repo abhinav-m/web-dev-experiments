7ad843044c7c8b984086e4120f6e77b1e20aced6
Abhinav Mishra <abhinav.thinktank@gmail.com>
2017-09-17 12:59:49 +0530

    Successfully tested movement in all directions

--- images-test/movement-test.js
+++ images-test/movement-test.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-  var MOV_INDEX;
+    var MOV_INDEX;
 
-  makeBoard();
-  setMover();
-  resetMovIndex();
+    makeBoard();
+    setMover();
+    resetMovIndex();
 
 });
 
 var MOVES = {
-  UP: 'up',
-  DOWN: 'down',
-  LEFT: 'left',
-  RIGHT:'right'
+    UP: 'up',
+    DOWN: 'down',
+    LEFT: 'left',
+    RIGHT: 'right'
 }
 
 var CUR_DIR = MOVES.DOWN;
@@ -25,46 +25,48 @@ right = 39
 down = 40 */
 
 function makeBoard() {
- var board =  $('.wrapper');
+    var board = $('.wrapper');
 
- for(var i = 0; i < 32 ; i++){
- board.append(`<div class="${ i==CUR_POS ? 'cell lord-down-0':'cell' }" id=${i}></div>`);
- }
+    for (var i = 0; i < 32; i++) {
+        board.append(`<div class="${ i==CUR_POS ? 'cell lord-down-0':'cell' }" id=${i}></div>`);
+    }
 
 }
 
 
 function resetMovIndex() {
-  MOV_INDEX = 0;
+    MOV_INDEX = 0;
 }
 
 
 function setMover() {
-  $(document).keydown(moveHandler);
+    $(document).keydown(moveHandler);
 }
 
 // keyboard key handing logic.
 function moveHandler(e) {
-  switch(e.which) {
-    case 38: move(MOVES.UP);
-    break;
-    case 40: move(MOVES.DOWN);
-    break;
-    case 37: move(MOVES.LEFT)
-    break;
-    case 39: move(MOVES.RIGHT);
-    break;
-    default : console.log('wrong key press');
-    break;
-  }
+    switch (e.which) {
+        case 38:
+            move(MOVES.UP);
+            break;
+        case 40:
+            move(MOVES.DOWN);
+            break;
+        case 37:
+            move(MOVES.LEFT)
+            break;
+        case 39:
+            move(MOVES.RIGHT);
+            break;
+        default:
+            console.log('wrong key press');
+            break;
+    }
 }
 
 function move(direction) {
-document.getElementById(CUR_POS).className = 'cell';
-switch(direction) {
-  case  'down' :  addClass(direction)
-                      break;
-}
+    document.getElementById(CUR_POS).className = 'cell';
+    addClass(direction)
 
 
 }
@@ -72,21 +74,68 @@ switch(direction) {
 
 function addClass(direction) {
 
-  switch(direction) {
-    case 'down': if(CUR_DIR === MOVES.DOWN) {
-      var className = `cell lord-down-${MOV_INDEX+1}`;
-      MOV_INDEX += 1 ;
-      if(MOV_INDEX === 4)
-      MOV_INDEX = 0;
-      document.getElementById((CUR_POS+8)).className = className;
+    switch (direction) {
+
+        case MOVES.UP:
+            if (CUR_DIR === MOVES.UP) {
+                var className = `cell lord-up-${MOV_INDEX+1}`;
+                MOV_INDEX += 1;
+                if (MOV_INDEX === 4)
+                    resetMovIndex();
+                document.getElementById((CUR_POS - 8)).className = className;
+            } else {
+                resetMovIndex();
+                CUR_DIR = MOVES.UP;
+                document.getElementById((CUR_POS - 8)).className = `cell lord-up-${MOV_INDEX}`;
+            }
+            CUR_POS -= 8;
+            break;
+
+        case MOVES.DOWN:
+            if (CUR_DIR === MOVES.DOWN) {
+                var className = `cell lord-down-${MOV_INDEX+1}`;
+                MOV_INDEX += 1;
+                if (MOV_INDEX === 4)
+                    resetMovIndex();
+                document.getElementById((CUR_POS + 8)).className = className;
+            } else {
+                resetMovIndex();
+                CUR_DIR = MOVES.DOWN;
+                document.getElementById((CUR_POS + 8)).className = `cell lord-down-${MOV_INDEX}`;
+            }
+            CUR_POS += 8;
+            break;
+
+        case MOVES.LEFT:
+            if (CUR_DIR === MOVES.LEFT) {
+                var className = `cell lord-left-${MOV_INDEX+1}`;
+                MOV_INDEX += 1;
+                if (MOV_INDEX === 4)
+                    resetMovIndex();
+                document.getElementById((CUR_POS - 1)).className = className;
+            } else {
+                resetMovIndex();
+                CUR_DIR = MOVES.LEFT;
+                document.getElementById((CUR_POS - 1)).className = `cell lord-left-${MOV_INDEX}`;
+            }
+            CUR_POS -= 1;
+            break;
+
+
+        case MOVES.RIGHT:
+            if (CUR_DIR === MOVES.RIGHT) {
+                var className = `cell lord-right-${MOV_INDEX+1}`;
+                MOV_INDEX += 1;
+                if (MOV_INDEX === 4)
+                    resetMovIndex();
+                document.getElementById((CUR_POS + 1)).className = className;
+            } else {
+                resetMovIndex();
+                CUR_DIR = MOVES.DOWN;
+                document.getElementById((CUR_POS + 1)).className = `cell lord-right-${MOV_INDEX}`;
+            }
+            CUR_POS += 1;
+            break;
     }
-    else {
-      resetMovIndex();
-      CUR_DIR = MOVES.DOWN;
-      document.getElementById((CUR_POS+8)).className = `cell lord-down-${MOV_INDEX}`;
-    }
-    CUR_POS += 8;
-    break;
-  }
 
 }
