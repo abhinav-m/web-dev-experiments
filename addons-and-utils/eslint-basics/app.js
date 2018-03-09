301f0f1b27c200c2732b74a6df29c8224d2fa1c8
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-03-09 23:42:41 +0530

    Added alot of changes,  eslint dummy project

---
+++ addons-and-utils/eslint-basics/app.js
@@ -0,0 +1,9 @@
+const bar = 10;
+const foo = bar + 20;
+
+const obj = {
+    name: 'Andrew',
+    age: 26
+};
+
+document.write(foo);
---
+++ addons-and-utils/eslint-basics/test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const jsx = <p>test</p>;
+var abcd = 10;
+ReactDOM.render(jsx, document.getElementById('root'));
